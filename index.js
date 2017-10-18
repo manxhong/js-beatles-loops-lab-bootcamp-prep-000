fbc4b7f559688e7f4e9000bd710faca086f11fce
Man Hong Lee <manhong323@gmail.com>
2017-10-18 18:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 console.log(theBeatlesPlay(musicians, instruments));
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var factLength = facts.length;
+  var arr = [];
+  while (factLength > 0) {
+    
+  }
+}
